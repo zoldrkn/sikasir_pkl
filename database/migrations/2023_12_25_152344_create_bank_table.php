9d084f050efde3c102b2fb582400d158fae4dc7d
ADINDA NATASYA 1002 <adinda.natasya@mhs.politala.ac.id>
2023-12-25 23:29:15 +0800

    dah rapiii

--- database/migrations/2023_12_25_152344_create_bank_table.php
+++ database/migrations/2023_12_25_152344_create_bank_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->date('tanggal_setoran');
             $table->text('keterangan');
+            $table->enum('pembayaran', ['cash', 'kredit']);
             $table->decimal('setoran_bank', 15,2);
             $table->timestamps();
         });
